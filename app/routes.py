4dcfe20c6b086655f6773df070310a3d26e985cd
rusql <russell.subscriptions@gmail.com>
2026-07-11 14:03:31 +0200

    Add Templates

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,17 @@ from app import app_instance
 @app_instance.route('/')
 @app_instance.route('/index')
 def index():
-    user  = {'username': 'Bob'} #creating a mock user
-    return render_template('index.html', title='Home', user=user)
+    user  = {'username': 'Russell'} #creating a mock user
+
+    posts = [
+        {
+            'author': {'username':'Russell'},
+            'body': 'Cool, clear day in Cape Town'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body': 'Camps bay is great'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
